5df74aa3f4757e8a862c2f615b2022b6ca9e7361
Felipe Polido <felipe.polido@iit.it>
2016-06-30 12:34:21 +0200

    ExampleLogger cleaning

--- src/ExampleLogger.cpp
+++ src/ExampleLogger.cpp
@@ -14,19 +14,18 @@ int main(int argc, char **argv)
 	BatchLogger logger;
 	double double_data = 0.0;
 	double local_time = 0.0;
-	std::vector<double> sine_waves(5, 0.0);
+	std::vector<double> sine_waves(5, 0.0); //Create vector of 5 signals
 	int count = 0;
 	float sawtooth_wave = 0.0;
 	float triangle_wave = 0.0;
 
 
-	static const double period = 0.001;//1/Sec
 	static const int log_time = 5;//Sec
-	static const int max_count = (int)(log_time/period);//Assume the topic is being published at 100 Hz
+	static const double period = 0.001;//1/Sec
+	static const int max_count = (int)(log_time/period);
 	std::cout << "Logging time is " << log_time << " sec" << std::endl;
 	std::cout << "Period is       " << period << " 1/sec" <<std::endl;
 	std::cout << "Sample count of " << max_count << std::endl;
-
 
 	if (!logger.hasInited()) {
 		logger.init(period);
@@ -44,42 +43,47 @@ int main(int argc, char **argv)
 	logger.add_datapoint("sawtooth_wave","s",&sawtooth_wave);
 	logger.add_datapoint("triangle_wave","s",&triangle_wave);
 	
-
 	auto t_start = std::chrono::high_resolution_clock::now();
 
 	while(1){
 
-
+		//Forever increment value:
 		double_data += 0.01;
 
+		//Get local time
 		auto t_latest = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double> diff = t_latest - t_start;
 		local_time = diff.count();
-		
+	
+		//Random sine wave generation:
 		//Amplitude*cos(2*M_PI*freq*time + phase);
 		sine_waves[0] = 1.0*cos(2*M_PI*10*local_time);
-		sine_waves[1] = 1.0*cos(2*M_PI*100*local_time);
+		sine_waves[1] = 1.0*cos(2*M_PI*50*local_time);
 		sine_waves[2] = 1.0*cos(2*M_PI*5*local_time);
 		sine_waves[3] = 1.0*cos(2*M_PI*2*local_time);
 		sine_waves[4] = 1.0*cos(2*M_PI*1*local_time);
 		
-		//Sawtooth wave gen:
+		//Sawtooth wave generation:
 		sawtooth_wave = abs((count % 100) - 1);
 
-		//Triangle wave gen:
-		int triangle_period = 100;
-		triangle_wave = (1.0/triangle_period) * 
-			(triangle_period - abs(count % (2*triangle_period) - triangle_period));
+		//Triangle wave generation:
+		int tri_half_period = 100;
+		triangle_wave = (1.0/tri_half_period) * 
+			(tri_half_period - abs(count % (2*tri_half_period) - tri_half_period));
 		
+		//Capture data ( relis on loop happening at the right intervals)
 		logger.saveData();
 
-
-		if(count == max_count) break;
+		//If number of max logs is reached, stop
+		if(count == max_count) break; 
 		count++;
-		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<long>(1000*period)));
+
+		std::this_thread::sleep_for(std::chrono::microseconds(static_cast<long>(1000000*period)));
+
 		if(count%100 == 0) std::cout << "Count " << count << " Time " << local_time << std::endl;
 	}
 
+	//Save log to file (with pre-fix "ctrl") in folder /logs/mrdplot
 	logger.writeToMRDPLOT2("ctrl");
 	return 0;
 }
