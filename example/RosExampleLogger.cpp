62a4d03c8a627d7cbf47190914d3d8c56c5c1fb0
Felipe Polido <felipe.polido@iit.it>
2016-07-04 16:19:25 +0200

    Pattern keeping ( terrible for checking history in git)

--- example/RosExampleLogger.cpp
+++ example/RosExampleLogger.cpp
@@ -14,46 +14,49 @@ bool stop = false;
 
 void chatterCallback(const std_msgs::Float64::ConstPtr& msg)
 {
-	//Update variables:
-	double_data =  msg->data;
-	local_time = ros::Time::now().toNSec();
-	//Save the data
-	logger.saveData();
+    //Update variables:
+    double_data =  msg->data;
+    local_time = ros::Time::now().toNSec();
+    //Save the data
+    logger.saveData();
 
-	static int count = 0;
-	count ++;
-	std::cout << "Logged " << double_data << " in position "
-		<< count << std::endl;
-	if(count == 300){ //Save X number of elements
-		//If we reached 1000 points, save the data into file
-		logger.writeToMRDPLOT2("ctrl");
-		stop = true;
-	}
+    static int count = 0;
+    count ++;
+    std::cout << "Logged " << double_data << " in position "
+        << count << std::endl;
+    if (count == 300)
+    { //Save X number of elements
+      //If we reached X number of points, save the data into file
+      logger.writeToMRDPLOT2("ctrl");
+      stop = true;
+    }
 
 } 
 
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "listener");
 
-	double period = 0.01;//Assume the topic is being published at 100 Hz
-	if (!logger.hasInited()) {
-		logger.init(period);
-	}
+  double period = 0.01;//Assume the topic is being published at 100 Hz
+  if (!logger.hasInited())
+  {
+    logger.init(period);
+  }
 
-	// Add variables to be logged:
-	logger.add_datapoint("double_data","s",&double_data);
-	logger.add_datapoint("local_time","s",&local_time);
-	
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  // Add variables to be logged:
+  logger.add_datapoint("double_data","s",&double_data);
+  logger.add_datapoint("local_time","s",&local_time);
 
-	while(1){
-		ros::spinOnce();
-		if(stop) break;
-	}
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 
-	return 0;
+  while(1)
+  {
+    ros::spinOnce();
+    if(stop) break;
+  }
+
+  return 0;
 }
 
